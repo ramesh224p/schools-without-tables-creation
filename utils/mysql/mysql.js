643ba0da0682bf509bf039818a9775b835435ca1
ramesh224p <60536968+ramesh224p@users.noreply.github.com>
2021-03-17 22:56:38 +0530

    Update mysql.js

--- utils/mysql/mysql.js
+++ utils/mysql/mysql.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     'host' : 'localhost',
     'user' : 'root',
     'password' : '0987',
-    'database' : 'school'
+    'database' : 'schools'
 })
 
 connection.connect(function(err){
